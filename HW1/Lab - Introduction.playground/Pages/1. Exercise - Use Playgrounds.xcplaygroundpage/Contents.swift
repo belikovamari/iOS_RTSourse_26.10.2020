914b2cfa7091ebdac0569fd0bb3606534e53cf14
Maria <chugrina.mar@yandex.ru>
2020-11-05 17:36:38 +0300

    HW1

---
+++ HW1/Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -0,0 +1,32 @@
+/*:
+ ## Упражнение — использование Playgrounds
+ 
+ Код ниже печатает несколько коротких предложений о том, что вы узнали на этом уроке.  Откройте область консоли и посмотрите на результат вывода.
+ */
+print("Мы узнали следующее: История Swift, какое оборудование необходимо, о переменных, константах, типах данных")
+print("Какие особенности делают Swift современным и безопасным языком: Swift с каждым годом только набирает обороты, имеет строгую типизацию")
+print("Как использовать Swift REPL в терминале: Откройте Terminal. Введите swift и нажмите Enter. Введите print(“Здравствуй, мир!”) и нажмите Enter. Введите :exit и нажмите Enter. Выйдите из терминала")
+print("Как использовать playgrounds, чтобы быстро проверять код Swift: воспользоваться областью результатов и консольюс")
+
+/*:
+ Теперь выведите ваши собственные фразы в консоли. Выберите одну из ваших любимых песен. Используйте ваше знание функции «print», чтобы отобразить название и исполнителя песни.
+ */
+let song: String = "Yesterday"
+let singer: String = "The Beatles"
+print("Любимая песня", song,"исполнителя", singer)
+/*:
+ Используйте несколько функций «print», чтобы написать несколько строчек песни.
+ */
+print("Yesterday, all my troubles seemed so far away\nNow it looks as though they're here to stay\nOh, I believe in yesterday")
+
+
+/*:
+ 
+ _Copyright © 2017 Apple Inc._
+ 
+ _Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:_
+ 
+ _The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software._
+ 
+ _THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE._
+ */
